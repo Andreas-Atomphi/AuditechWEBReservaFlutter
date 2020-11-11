d56b9ea3c9c2ea18392e3caae0da2f398e779957
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-11-11 04:53:44 -0300

    [FEAT] CardPaciente aprimorada.
    Agora tem 4 estados: Novo, Normal, Alerta e Desativado.
    Só muda nas cores para ajudar o clínico, mas os estados possíveis de paciente continuam sendo Ativo e inativo (true ou false).

--- lib/Telas/Principal/Pacientes/Components/CardPaciente.dart
+++ lib/Telas/Principal/Pacientes/Components/CardPaciente.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutterweb/Telas/global/components.dart';
 
-enum TypePaciente {
-  ATIVADO,
+enum NovidadesPaciente {
+  NORMAL,
   ALERTA,
+  NOVO,
   DESATIVADO,
 }
 
-extension ColorPaciente on TypePaciente {
+extension ValuesPaciente on NovidadesPaciente {
   List<Color> get color {
     List<Color> toReturn;
     switch (this) {
-      case TypePaciente.ALERTA:
+      case NovidadesPaciente.ALERTA:
         toReturn = [
           Color(0xffffc107),
           Color(0xffffffff),
         ];
         break;
-      case TypePaciente.ATIVADO:
-        toReturn = [Color(0xffffffff), Color(0xff343a40)];
+      case NovidadesPaciente.NORMAL:
+        toReturn = [
+          Color(0xffffffff),
+          Color(0xff343a40),
+        ];
         break;
-      case TypePaciente.DESATIVADO:
+      case NovidadesPaciente.DESATIVADO:
         toReturn = [
           Color(0xff343a40),
+          Color(0xff778491),
+        ];
+        break;
+      case NovidadesPaciente.NOVO:
+        toReturn = [
+          Color(0xff75e900),
           Color(0xffffffff),
         ];
         break;
+    }
+    return toReturn;
+  }
+
+  String get texto {
+    String toReturn;
+    switch (this) {
+      case NovidadesPaciente.ALERTA:
+        toReturn = "Alerta";
+        break;
+      case NovidadesPaciente.NORMAL:
+        toReturn = "Normal";
+        break;
+      case NovidadesPaciente.DESATIVADO:
+        toReturn = "Desativado";
+        break;
+      case NovidadesPaciente.NOVO:
+        toReturn = "Novo";
+        break;
+    }
+    return toReturn;
+  }
+
+  bool get status {
+    bool toReturn;
+    switch (this) {
+      case NovidadesPaciente.DESATIVADO:
+        toReturn = false;
+        break;
+      default:
+        toReturn = true;
     }
     return toReturn;
   }
@@ -40,55 +82,83 @@ extension MapSpreadWidget on Map<String, String> {
   }
 }
 
-class CardPaciente extends StatelessWidget {
+class CardPaciente extends Card {
   final EdgeInsets padding;
-  final TypePaciente usuarioEsta;
+  final NovidadesPaciente usuarioEsta;
   final Icon icone;
-  final Map<String, String> atributos;
+  final Map<String, String> dados;
+  final String title;
 
   CardPaciente({
-    this.padding,
-    this.usuarioEsta,
-    this.icone,
-    this.atributos = const {
+    this.padding = const EdgeInsets.all(20),
+    this.usuarioEsta = NovidadesPaciente.NORMAL,
+    this.title,
+    this.icone = const Icon(
+      Icons.person,
+      size: 20,
+    ),
+    this.dados = const {
       "atributo": "valor",
     },
   });
 
   @override
   Widget build(BuildContext context) {
-    TextStyle _styleMe = TextStyle(
-      fontFamily: "OpenSans",
-      color: usuarioEsta.color[1],
-      fontSize: 20,
-      fontWeight: FontWeight.bold,
-    );
-    return Container(
+    Text _cardBody(String text) => Text(
+          text,
+          style: FormatoTexto.tituloCard(
+            cor: usuarioEsta.color[1],
+            weight: FontWeight.bold,
+          ),
+        );
+    Text _cardTitle(String text) => Text(
+          text,
+          style: FormatoTexto.tituloCard(
+            cor: usuarioEsta.color[1],
+            weight: FontWeight.bold,
+          ),
+        );
+    Map statusIcon = {
+      NovidadesPaciente.DESATIVADO: Icons.person,
+      NovidadesPaciente.NORMAL: Icons.person,
+      NovidadesPaciente.NOVO: Icons.new_releases,
+      NovidadesPaciente.ALERTA: Icons.warning,
+    };
+    return ContainerCard(
       padding: EdgeInsets.all(20),
-      color: usuarioEsta.color[0],
-      child: Column(
+      backgroundColor: usuarioEsta.color[0],
+      title: (title != null) ? _cardTitle(title) : null,
+      header: Stack(
         children: [
           Icon(
-            Icons.person,
+            statusIcon[usuarioEsta],
             color: usuarioEsta.color[1],
             size: 100,
           ),
-          ...atributos.mapToWidget(
-            (key, value) => Row(
-              children: [
-                Text(
-                  "$key: ",
-                  style: _styleMe,
-                ),
-                Text(
-                  "$value",
-                  style: _styleMe,
-                ),
-              ],
-            ),
-          )
+          if (usuarioEsta == NovidadesPaciente.NOVO)
+            Text(
+              "(NOVO)",
+              style: TextStyle(
+                fontFamily: "OpenSans",
+                color: Colors.pink[700],
+                fontWeight: FontWeight.bold,
+              ),
+            )
         ],
       ),
+      body: <Widget>[
+        ...dados.mapToWidget(
+          (key, value) => SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                _cardBody("$key: "),
+                _cardBody("$value"),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/Telas/Principal/Pacientes/Pacientes.dart
+++ lib/Telas/Principal/Pacientes/Pacientes.dart
@@ -4,21 +4,52 @@ import 'package:flutterweb/Telas/Principal/Pacientes/components.dart';
 class _SPacientes extends State<Pacientes> {
   @override
   Widget build(BuildContext context) {
+    NovidadesPaciente statusPaciente = NovidadesPaciente.NOVO;
+    bool statusTexto = statusPaciente.status;
+    String statusUsuario = {
+      true: "Ativo",
+      false: "Inativo",
+    }[statusTexto];
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: Container(
           padding: EdgeInsets.all(50),
           child: GridView.extent(
-            maxCrossAxisExtent: 250,
+            crossAxisSpacing: 20,
+            maxCrossAxisExtent: 255,
             children: [
               CardPaciente(
-                padding: EdgeInsets.all(20),
-                usuarioEsta: TypePaciente.ATIVADO,
-                icone: Icon(
-                  Icons.person_outline,
-                  size: 20,
-                ),
+                usuarioEsta: statusPaciente,
+                title: "AAAAAAAA",
+                dados: {
+                  "Status": statusUsuario,
+                  "Tratamento": "aaaa",
+                },
+              ),
+              CardPaciente(
+                usuarioEsta: NovidadesPaciente.NORMAL,
+                title: "AAAAAAAA",
+                dados: {
+                  "Status": "Ativo",
+                  "Tratamento": "aaaa",
+                },
+              ),
+              CardPaciente(
+                usuarioEsta: NovidadesPaciente.ALERTA,
+                title: "AAAAAAAA",
+                dados: {
+                  "Status": "Ativo",
+                  "Tratamento": "aaaa",
+                },
+              ),
+              CardPaciente(
+                usuarioEsta: NovidadesPaciente.DESATIVADO,
+                title: "AAAAAAAA",
+                dados: {
+                  "Status": "Ativo",
+                  "Tratamento": "aaaa",
+                },
               ),
             ],
           ),
